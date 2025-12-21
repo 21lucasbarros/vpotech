0069ad7dad41db9dc7b5f79aab8a1d3e917635d3
Lucas Barros Simon <lucasweacked21@gmail.com>
2025-12-21 11:57:23 -0300

    feat: criacao do projeto inicial.

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import "./app.css";
+import Header from "./components/header";
+
+export default function App() {
+  return (
+    <main className="bg-[#1a1a1a] flex flex-col min-h-screen">
+      <Header />
+    </main>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,40 @@
+import { scrollToElement } from "../utils/scrollToElement";
+
+export default function Header() {
+  return (
+    <header className="px-40 py-8 flex flex-row justify-between items-center baseline">
+      <h1 className="text-[#f7f7f7]"></h1>
+      <nav className="bg-zinc-400/50 px-8 py-2 rounded-2xl">
+        <ul className="flex justify-center gap-12 items-center">
+          <li>
+            <a
+              href="#about"
+              className="text-[#f7f7f7] hover:underline"
+              onClick={scrollToElement}
+            >
+              Sobre
+            </a>
+          </li>
+          <li>
+            <a
+              href="#services"
+              className="text-[#f7f7f7] hover:underline"
+              onClick={scrollToElement}
+            >
+              Projetos
+            </a>
+          </li>
+          <li>
+            <a
+              href="#contact"
+              className="text-[#f7f7f7] hover:underline"
+              onClick={scrollToElement}
+            >
+              Contato
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
